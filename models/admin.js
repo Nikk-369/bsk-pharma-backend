6ec71d06d1b689ed97e03387f7889d83853914b4
Ankita-Sehrawat <sehrawatankita1@gmail.com>
2025-09-18 11:55:46 +0530

    for mail

--- models/admin.js
+++ models/admin.js
@@ -1,3 +1,26 @@
+// const mongoose = require("mongoose");
+
+// const adminSchema = mongoose.Schema(
+//   {
+//     email: { type: String, unique: true, required: true },
+//     password: { type: String, required: true },
+//     role: { type: String, default: "User" }, // Default role set here
+//     name: { type: String },
+//     image: { type: String },
+//     phone: { type: String },
+//     // address: { type: String },
+//     address: { type: [String], default: [] },
+//     location: { type: String },
+//     timeStamp: { type: String },
+//     deleted_at: { type: Date, default: null },
+//   },
+//   { timestamps: true }
+// );
+
+// module.exports = mongoose.model("Admin", adminSchema);
+
+
+// // 2:
 const mongoose = require("mongoose");
 
 const adminSchema = mongoose.Schema(
@@ -18,3 +41,4 @@ const adminSchema = mongoose.Schema(
 );
 
 module.exports = mongoose.model("Admin", adminSchema);
+
